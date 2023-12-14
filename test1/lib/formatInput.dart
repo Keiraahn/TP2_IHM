59ea8654646afea317a3235536a46d5f3d096590
Keiraahn <dzaniightmare@gmail.com>
2023-12-14 14:00:36 +0100

    Carte finit 100%

--- test1/lib/formatInput.dart
+++ test1/lib/formatInput.dart
@@ -7,41 +7,35 @@ class FonctionFormatInputNumeroCarte extends TextInputFormatter {
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     String newText = newValue.text.replaceAll(' ', '');
-    if (getCardType(newText) == 'amex') {
-      if (newText.length <= 15) {
-        StringBuffer buffer = StringBuffer();
-        for (int i = 0; i < newText.length; i++) {
-          buffer.write(newText[i]);
-          if (i == 3 || i == 9) {
-            buffer.write(' ');
-          }
-        }
+    StringBuffer buffer = StringBuffer();
 
-        String number = buffer.toString();
-        return newValue.copyWith(
-          text: number,
-          selection: TextSelection.collapsed(offset: number.length),
-        );
-      }
-    } else {
-      if (newText.length <= 16) {
-        StringBuffer buffer = StringBuffer();
-        for (int i = 0; i < newText.length; i++) {
-          buffer.write(newText[i]);
-          int saut = i + 1;
-          if (saut % 4 == 0 && newText.length > newText.length - 4) {
-            buffer.write(' ');
-          }
-        }
+    bool isDeleting = newValue.text.length < oldValue.text.length;
+    int lastNonSpaceIndex = oldValue.text.length - 1;
+    while (lastNonSpaceIndex >= 0 && oldValue.text[lastNonSpaceIndex] == ' ') {
+      lastNonSpaceIndex--;
+    }
 
-        String number = buffer.toString();
-        return newValue.copyWith(
-          text: number,
-          selection: TextSelection.collapsed(offset: number.length),
-        );
+    for (int i = 0; i < newText.length; i++) {
+      buffer.write(newText[i]);
+      int nextIndex = i + 1;
+      if (getCardType(newText) == 'amex') {
+        if ((i == 3 || i == 9) &&
+            !(isDeleting && nextIndex >= lastNonSpaceIndex)) {
+          buffer.write(' ');
+        }
+      } else {
+        if (nextIndex % 4 == 0 &&
+            nextIndex != newText.length &&
+            !(isDeleting && nextIndex >= lastNonSpaceIndex)) {
+          buffer.write(' ');
+        }
       }
     }
 
-    return oldValue;
+    String formattedText = buffer.toString();
+    return newValue.copyWith(
+      text: formattedText,
+      selection: TextSelection.collapsed(offset: formattedText.length),
+    );
   }
 }
